d889054f1c155cdad1b015040206b4766b0c8d2f
Artem <alefa2000@mail.ru>
2026-01-22 10:33:17 +0300

    service

--- backend/internal/application/abstraction/repository/game_repository.go
+++ backend/internal/application/abstraction/repository/game_repository.go
@@ -8,13 +8,13 @@ import (
 )
 
 type GameRepository interface {
-	Create(ctx context.Context, game *model.Game) error
+	Create(ctx context.Context, game *model.Game) (*model.Game, error)
 
 	FindByID(ctx context.Context, id uuid.UUID) (*model.Game, error)
 
 	FindAll(ctx context.Context, limit, offset int) ([]*model.Game, error)
 
-	Update(ctx context.Context, game *model.Game) error
+	Update(ctx context.Context, game *model.Game) (*model.Game, error)
 
 	Delete(ctx context.Context, id uuid.UUID) error
 
--- backend/internal/application/services/game_service.go
+++ backend/internal/application/services/game_service.go
@@ -4,54 +4,56 @@ import (
 	"context"
 	"errors"
 	"example/web-service-gin/internal/application/abstraction/repository"
-	"example/web-service-gin/internal/domain/model"
+	"example/web-service-gin/internal/application/dto"
+	"example/web-service-gin/internal/application/mapper"
 	"time"
-
-	"github.com/google/uuid"
 )
 
 type GameService struct {
-	repo repository.GameRepository
+	repo       repository.GameRepository
+	gameMapper *mapper.GameMapper
 }
 
 func NewGameService(repo repository.GameRepository) *GameService {
 	return &GameService{
-		repo: repo,
+		repo:       repo,
+		gameMapper: mapper.NewGameMapper(),
 	}
 }
 
 func (s *GameService) CreateGame(ctx context.Context,
-	title, description string,
-	releaseDate time.Time,
-	genreID uuid.UUID) (*model.Game, error) {
+	gameCreateDto dto.CreateGameDto) (*dto.GameDto, error) {
 
-	if err := s.validateGameData(title, description, releaseDate); err != nil {
+	if err := s.validateGameData(
+		gameCreateDto); err != nil {
 		return nil, err
 	}
 
-	game, err := model.NewGameWithValidate(title, description, releaseDate, genreID)
+	game, mapperError := s.gameMapper.FromCreateGameDto(&gameCreateDto)
 
-	if err != nil {
-		return nil, err
+	if mapperError != nil {
+		return nil, mapperError
 	}
 
-	if err := s.repo.Create(ctx, game); err != nil {
-		return nil, err
+	createdGame, repoError := s.repo.Create(ctx, game)
+
+	if repoError != nil {
+		return nil, repoError
 	}
 
-	return game, nil
+	return s.gameMapper.ToGameDto(createdGame), nil
 }
 
-func (s *GameService) validateGameData(title, description string, releaseDate time.Time) error {
-	if title == "" || len(title) > 200 {
+func (s *GameService) validateGameData(gameDto dto.CreateGameDto) error {
+	if gameDto.Title == "" || len(gameDto.Title) > 200 {
 		return errors.New("title must be between 1 and 200 characters")
 	}
 
-	if len(description) > 2000 {
+	if len(gameDto.Description) > 2000 {
 		return errors.New("description too long")
 	}
 
-	if releaseDate.After(time.Now().AddDate(1, 0, 0)) {
+	if gameDto.ReleaseDate.After(time.Now().AddDate(1, 0, 0)) {
 		return errors.New("release date too far in the future")
 	}
 
