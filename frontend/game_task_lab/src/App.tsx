fcfdf39bf09cb52babcd64882a272a138e408f2e
Artem <alefa2000@mail.ru>
2026-01-23 13:36:38 +0300

    router

--- frontend/game_task_lab/src/App.tsx
+++ frontend/game_task_lab/src/App.tsx
@@ -1,13 +1,18 @@
 import "./App.css";
+import { Route, Router } from "@solidjs/router";
 import { AppLayout } from "./assets/AppLayout.tsx";
-import { GamesPage } from "./features/games/pages/GamesPage";
+import { GamesPage } from "./features/games/pages/GamesPage.tsx";
 
 function App() {
-  return (
-    <AppLayout>
-      <GamesPage />
-    </AppLayout>
-  );
+    return (
+        <Router>
+            <Route path="/" component={() => (
+                <AppLayout showSidebar={true}>
+                    <GamesPage />
+                </AppLayout>
+            )} />
+        </Router>
+    );
 }
 
 export default App;
--- frontend/game_task_lab/src/assets/AppLayout.tsx
+++ frontend/game_task_lab/src/assets/AppLayout.tsx
@@ -4,12 +4,15 @@ import { Sidebar } from "../shared/components/sidebar/Sidebar.tsx";
 
 interface AppLayoutProps {
   children: JSX.Element;
+  showSidebar?: boolean;
 }
 
 export const AppLayout = (props: AppLayoutProps) => {
+  const showSidebar = props.showSidebar ?? true;
+
   return (
     <div class="app-shell">
-      <Sidebar />
+      {showSidebar && <Sidebar />}
       <main class="app-main app-content">{props.children}</main>
     </div>
   );
